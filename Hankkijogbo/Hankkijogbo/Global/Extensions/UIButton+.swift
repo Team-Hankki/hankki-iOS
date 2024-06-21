2fc8faa6e029da0604602dd80d8724e255724f79
mcrkgus <mcrkgus@naver.com>
2024-06-21 17:05:18 +0900

    [Setting] #1 - Extension 추가

---
+++ Hankkijogbo/Hankkijogbo/Global/Extensions/UIButton+.swift
@@ -0,0 +1,36 @@
+//
+//  UIButton+.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 2024/06/21.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    /// Button 모서리 커스텀 - 색상, 모서리 두께, 둥근 정도
+    func makeRoundBorder(cornerRadius: CGFloat, borderWidth: CGFloat, borderColor: UIColor ) {
+        layer.masksToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+    }
+    
+    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
+        let minimumSize: CGSize = CGSize(width: 1.0, height: 1.0)
+        
+        UIGraphicsBeginImageContext(minimumSize)
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(origin: .zero, size: minimumSize))
+        }
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.clipsToBounds = true
+        self.setBackgroundImage(colorImage, for: state)
+    }
+}
---
+++ Hankkijogbo/Hankkijogbo/Global/Extensions/UIView+.swift
@@ -0,0 +1,65 @@
+//
+//  UIView+.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 2024/06/21.
+//
+
+import UIKit
+
+extension UIView {
+    
+    /// 한 번에 여러 개의 UIView 또는 UIView의 하위 클래스 객체들을 상위 UIView에 추가
+    func addSubviews(_ views: UIView...) {
+        views.forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    /// UIView 의 모서리가 둥근 정도를 설정
+    /// - Parameter radius: radius 값
+    /// - Parameter maskedCorners: radius를 적용할 코너 지정
+    func makeRounded(radius: CGFloat) {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+    }
+}
+
+extension UIView {
+    
+    // MARK: - 기기 대응
+    
+    func getDeviceWidth() -> CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    func getDeviceHeight() -> CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+    /// 아이폰 13 미니(width 375)를 기준으로 레이아웃을 잡고, 기기의 width 사이즈를 곱해 대응 값을 구할 때 사용
+    func convertByWidthRatio(_ convert: CGFloat) -> CGFloat {
+        return convert * (getDeviceWidth() / 375)
+    }
+    
+    /// 아이폰 13 미니(height 812)를 기준으로 레이아웃을 잡고, 기기의 height 사이즈를 곱해 대응 값을 구할 때 사용
+    func convertByHeightRatio(_ convert: CGFloat) -> CGFloat {
+        return convert * (getDeviceHeight() / 812)
+    }
+    
+    /// 아이폰 13 미니(width 375)를 기준으로 레이아웃을 잡고, 기기의 width 사이즈를 곱해 대응 값을 구할 때 사용
+    func convertByReverseWidthRatio(_ convert: CGFloat) -> CGFloat {
+        return convert * (375 / getDeviceWidth())
+    }
+    
+    /// 아이폰 13 미니(height 812)를 기준으로 레이아웃을 잡고, 기기의 height 사이즈를 곱해 대응 값을 구할 때 사용
+    func convertByReverseHeightRatio(_ convert: CGFloat) -> CGFloat {
+        return convert * (812 / getDeviceHeight())
+    }
+    
+    /// 노치가 있는지 없는지 Bool 값 반환
+    var hasNotch: Bool {
+        return !( (UIScreen.main.bounds.width / UIScreen.main.bounds.height) > 0.5 )
+    }
+}
