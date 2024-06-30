239644cf8938fb4b79904168e2185f0cb50e3e42
EunsuSeo01 <eunsu0613@naver.com>
2024-06-30 19:17:13 +0900

    [Setting] #5 - Base 파일 추가

---
+++ Hankkijogbo/Hankkijogbo/Present/Base/BaseTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  BaseTableViewCell.swift
+//  Hankkijogbo
+//
+//  Created by 서은수 on 6/30/24.
+//
+
+import UIKit
+
+/// 모든 UITableViewCell는 BaseTableViewCell를 상속 받는다.
+/// - 각 함수를 override하여 각 Cell에 맞게 함수 내용을 작성한다.
+/// - 각 Cell에서는 해당 함수들을 호출하지 않아도 된다.
+class BaseTableViewCell: UITableViewCell {
+    
+    // MARK: - Init
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupStyle()
+        setupLayout()
+        setupHierarchy()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Set UI
+
+    func setupStyle() { }
+    
+    func setupLayout() { }
+    
+    func setupHierarchy() { }
+}
---
+++ Hankkijogbo/Hankkijogbo/Present/Base/BaseViewController.swift
@@ -0,0 +1,47 @@
+//
+//  BaseViewController.swift
+//  Hankkijogbo
+//
+//  Created by 서은수 on 6/30/24.
+//
+
+import UIKit
+
+/// 모든 UIViewController는 BaseViewController를 상속 받는다.
+/// - 각 함수를 override하여 각 VC에 맞게 함수 내용을 작성한다.
+/// - 각 VC에서는 해당 함수들을 호출하지 않아도 된다.
+class BaseViewController: UIViewController {
+    
+    // MARK: - Init
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        setupStyle()
+        setupLayout()
+        setupHierarchy()
+    }
+
+    // MARK: - Set UI
+
+    func setupStyle() { }
+    
+    func setupLayout() { }
+    
+    func setupHierarchy() { }
+}
