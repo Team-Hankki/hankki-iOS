c82a87c7136c8dc294dd627769e7b72d824508c9
mcrkgus <mcrkgus@naver.com>
2024-07-06 01:11:17 +0900

    [Feat] #15 - NavigationType

---
+++ Hankkijogbo/Hankkijogbo/Global/Components/HankkiNavigationController/HankkiNavigationType.swift
@@ -0,0 +1,22 @@
+//
+//  HankkiNavigationType.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/6/24.
+//
+
+import UIKit
+
+struct HankkiNavigationType {
+    var hasBackButton: Bool     // backButton 존재 여부
+    var hasRightButton: Bool    // rightButton 존재 여부
+
+    var mainTitle: StringOrImageType        // mainTitle
+    var rightButton: StringOrImageType      // rightButton
+    var rightButtonAction: () -> Void   // rightButton의 액션
+}
+
+enum StringOrImageType {
+    case string(String)
+    case image(UIImage)
+}
