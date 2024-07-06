40d6fbedd20dc3ffa2760f0f0d171445bd3a09c0
shimseohyun <shim020924@gmail.com>
2024-07-07 01:40:07 +0900

    [Feat] #16 - viewcontroller extension 추가

--- Hankkijogbo/Hankkijogbo/Present/ViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/ViewController.swift
@@ -1,39 +1,16 @@
-import UIKit
-import Then
+//
+//  ViewController.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 2024/06/21.
+//
 
-class ViewController: BaseViewController {
-    
+import UIKit
+
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 버튼 생성
-        let button = UIButton(type: .system)
-        button.setTitle("Show Alert", for: .normal)
-        button.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
-        
-        // 버튼 레이아웃 설정
-        button.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button)
-        NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
-    
-    @objc func showAlert() {
-        let alert = AlertViewController()
-        alert.modalPresentationStyle = .overFullScreen
-        alert.do {
-            $0.image = "2"
-            $0.titleText = "변동사항을 알려주셔서 감사합니다 :)\n저렴하고 든든한 식사하세요!"
-            $0.subText = "정말 로그아웃 하실 건가요?"
-            
-            $0.secondaryButtonText = "돌아가기"
-            $0.primaryButtonText = "로그아웃"
-
-        }
-        self.present(alert, animated: false, completion: nil)
+        // Do any additional setup after loading the view.
     }
 }
-
-
