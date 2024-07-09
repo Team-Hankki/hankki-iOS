467ffbca3138d2af0484b7d43c509384992aff29
mcrkgus <mcrkgus@naver.com>
2024-07-09 17:34:56 +0900

    [Chore] #22 - config file 생성

---
+++ Hankkijogbo/Config.swift
@@ -0,0 +1,32 @@
+//
+//  Config.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/9/24.
+//
+
+import Foundation
+
+enum Config {
+    enum Keys {
+        enum Plist {
+            static let NMFClientId = "NMFClientId"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found.")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    static let naverMapClientID: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.naverMapClientID] as? String else {
+            fatalError("ClientID is not set in plist for this configuration.")
+        }
+        return key
+    }()
+}
