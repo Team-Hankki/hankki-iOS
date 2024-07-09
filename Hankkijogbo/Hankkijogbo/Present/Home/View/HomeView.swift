c347c2b077b3744855eb7ccdd31e4d9d507637d3
mcrkgus <mcrkgus@naver.com>
2024-07-09 14:49:03 +0900

    [Feat] #22 - Map 연결 및 필터링 버튼 UI

--- Hankkijogbo/Hankkijogbo/Present/Home/View/HomeView.swift
+++ Hankkijogbo/Hankkijogbo/Present/Home/View/HomeView.swift
@@ -4,7 +4,85 @@
 //
 //  Created by Gahyun Kim on 2024/06/21.
 //
+import NMapsMap
 
 final class HomeView: BaseView {
     
+    // MARK: - Properties
+    
+    private let buttonType = ["종류", "가격대", "정렬"]
+    
+    // MARK: - UI Components
+    
+    var mapView = NMFMapView()
+    
+    let typeButton = UIButton()
+    let priceButton = UIButton()
+    let sortButton = UIButton()
+    
+    override func setupHierarchy() {
+        addSubviews(mapView, typeButton, priceButton, sortButton)
+    }
+    
+    override func setupLayout() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        typeButton.snp.makeConstraints {
+            $0.top.equalTo(mapView.snp.top).inset(22)
+            $0.leading.equalTo(mapView.snp.leading).inset(22)
+            $0.width.equalTo(61)
+            $0.height.equalTo(32)
+        }
+        
+        priceButton.snp.makeConstraints {
+            $0.top.equalTo(typeButton.snp.top)
+            $0.leading.equalTo(typeButton.snp.trailing).offset(8)
+            $0.width.equalTo(72)
+            $0.height.equalTo(32)
+        }
+        
+        sortButton.snp.makeConstraints {
+            $0.top.equalTo(typeButton.snp.top)
+            $0.leading.equalTo(priceButton.snp.trailing).offset(8)
+            $0.width.equalTo(61)
+            $0.height.equalTo(32)
+        }
+        
+    }
+    
+    override func setupStyle() {
+        let buttons = [typeButton, priceButton, sortButton]
+        for (index, button) in buttons.enumerated() {
+            button.do {
+                $0.backgroundColor = .white
+                $0.makeRoundBorder(cornerRadius: 16, borderWidth: 1, borderColor: .gray300)
+                $0.setTitle(buttonType[index], for: .normal)
+                $0.setTitleColor(.gray400, for: .normal)
+                $0.setImage(.icArrow, for: .normal)
+                $0.titleLabel?.font = .setupPretendardStyle(of: .caption1)
+                $0.contentHorizontalAlignment = .left
+                $0.semanticContentAttribute = .forceRightToLeft
+                $0.contentEdgeInsets = .init(top: 0, left: 12, bottom: 0, right: 0)
+                $0.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+            }
+        }
+    }
+}
+
+private extension HomeView {
+    @objc private func buttonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        updateButtonStyle(sender)
+    }
+    
+    func updateButtonStyle(_ button: UIButton) {
+        if button.isSelected {
+            button.setTitleColor(.gray600, for: .normal)
+           // button.setImage(.ic, for: <#T##UIControl.State#>)
+        } else {
+            button.setTitleColor(.gray400, for: .normal)
+        }
+    }
 }
--- Hankkijogbo/Hankkijogbo/Present/Home/View/HomeViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/Home/View/HomeViewController.swift
@@ -5,6 +5,125 @@
 //  Created by Gahyun Kim on 2024/06/21.
 //
 
+import UIKit
+
+import NMapsMap
+
 final class HomeViewController: BaseViewController {
     
+    private var rootView = HomeView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupMap()
+        setupPosition()
+    }
+    
+    override func setupHierarchy() {
+        view.addSubview(rootView)
+    }
+    
+    override func setupLayout() {
+        rootView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(50)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
 }
+
+// MARK: - MapView Setting
+extension HomeViewController {
+    func setupMap() {
+        rootView.mapView.touchDelegate = self
+    }
+    
+    func setupPosition() {
+        let initialPosition = NMGLatLng(lat: 37.5665, lng: 126.9780)
+        rootView.mapView.positionMode = .direction
+        rootView.mapView.moveCamera(NMFCameraUpdate(scrollTo: initialPosition))
+        
+        // 마커 추가
+        let markers = [
+            (lat: 37.5766102, lng: 126.9783881),
+            (lat: 37.5266102, lng: 126.9785881),
+            (lat: 37.5646102, lng: 126.9787881),
+            (lat: 37.5766152, lng: 126.9789881)
+        ]
+        
+        for (index, location) in markers.enumerated() {
+            let marker = NMFMarker()
+            marker.position = NMGLatLng(lat: location.lat, lng: location.lng)
+            marker.mapView = rootView.mapView
+            marker.touchHandler = { (overlay) in
+                print("Marker \(index + 1) clicked")
+                return true
+            }
+        }
+    }
+}
+
+extension HomeViewController: NMFMapViewCameraDelegate {}
+
+extension HomeViewController: NMFMapViewTouchDelegate {
+    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+        print("Map clicked at lat: \(latlng.lat), lng: \(latlng.lng)")
+    }
+}
+
+
+
+//final class HomeViewController: UIViewController, NMFMapViewTouchDelegate, NMFMapViewCameraDelegate {
+//
+//    private var mymapView = NMFMapView()
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        setupMap()
+//        setupPosition()
+//        
+//    }
+//    
+//    
+//    func setupMap() {
+//        mymapView =  NMFMapView(frame: view.frame)
+//        mymapView.touchDelegate = self
+//        
+//        view.addSubview(mymapView)
+//        mymapView.snp.makeConstraints {
+//            $0.edges.equalToSuperview().inset(20)
+//        }
+//    }
+//    
+//    func setupPosition() {
+//        let initialPosition = NMGLatLng(lat: 37.5665, lng: 126.9780)
+//        mymapView.positionMode = .direction
+//        mymapView.moveCamera(NMFCameraUpdate(scrollTo: initialPosition))
+//        
+//        // 마커 추가
+//        let markers = [
+//            (lat: 37.5766102, lng: 126.9783881),
+//            (lat: 37.5266102, lng: 126.9785881),
+//            (lat: 37.5646102, lng: 126.9787881),
+//            (lat: 37.5766152, lng: 126.9789881)
+//        ]
+//        
+//        for (index, location) in markers.enumerated() {
+//            let marker = NMFMarker()
+//            marker.position = NMGLatLng(lat: location.lat, lng: location.lng)
+//            marker.mapView = mymapView
+//            marker.touchHandler = { (overlay) in
+//                print("Marker \(index + 1) clicked")
+//                return true
+//            }
+//        }
+//    }
+//    
+//    // NMFMapViewTouchDelegate 메서드
+//    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+//        print("Map clicked at lat: \(latlng.lat), lng: \(latlng.lng)")
+//    }
+//}
+//
