8f95916d348545fd64232e5d67957f3753a8503a
mcrkgus <mcrkgus@naver.com>
2024-07-11 02:27:40 +0900

    [Feat] #47 - Type CollectionView

---
+++ Hankkijogbo/Hankkijogbo/Present/Home/View/Cell/TypeCollectionView.swift
@@ -0,0 +1,36 @@
+//
+//  TypeCollectionView.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/10/24.
+//
+
+import UIKit
+
+final class TypeCollectionView: BaseView {
+
+    private let flowLayout = UICollectionViewFlowLayout()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+
+    override func setupHierarchy() {
+        addSubview(collectionView)
+    }
+    
+    override func setupLayout() {
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(900)
+            $0.height.equalTo(100)
+        }
+    }
+    
+    override func setupStyle() {
+        flowLayout.do {
+            //$0.itemSize = .init(width: UIScreen.getDeviceWidth(), height: 100)
+            $0.minimumLineSpacing = 6
+            $0.scrollDirection = .horizontal
+        }
+        
+        collectionView.backgroundColor = .clear
+    }
+}
