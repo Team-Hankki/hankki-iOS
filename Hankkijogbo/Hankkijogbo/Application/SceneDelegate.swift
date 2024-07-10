67546df791925cecb09c0c7722dec84a91c21685
shimseohyun <shim020924@gmail.com>
2024-07-10 17:56:44 +0900

    [Feat] #37 - sence delegate 설정

--- Hankkijogbo/Hankkijogbo/Application/SceneDelegate.swift
+++ Hankkijogbo/Hankkijogbo/Application/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = /*TabBarController()*/ UnivSelectViewController()
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
     
