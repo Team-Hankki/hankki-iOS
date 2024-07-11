a80784581b58076c89465169b8fc284c7d8d215c
mcrkgus <mcrkgus@naver.com>
2024-07-11 22:30:24 +0900

    [Feat] #47 - DropDown : Button Type

---
+++ Hankkijogbo/Hankkijogbo/Global/Components/HankkiDropDown/ButtonType.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonType.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/11/24.
+//
+
+import Foundation
+
+enum ButtonType {
+    // case type
+    case price
+    case sort
+}
