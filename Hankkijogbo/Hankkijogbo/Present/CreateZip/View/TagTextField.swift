120c4454e6d49dbe4785e883c68784eed5346826
shimseohyun <shim020924@gmail.com>
2024-07-12 01:04:40 +0900

    [Feat] #51 - 복사 붙어넣기 안되게

--- Hankkijogbo/Hankkijogbo/Present/CreateZip/View/TagTextField.swift
+++ Hankkijogbo/Hankkijogbo/Present/CreateZip/View/TagTextField.swift
@@ -10,10 +10,12 @@ import UIKit
 class TagTextField: UITextField {
 
     // 터치 이벤트를 무시하여 드래그 방지
-    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        if gestureRecognizer is UIPanGestureRecognizer {
-            return false // 패닝(드래그) 제스처 무시
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+            return false
         }
-        return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        
+        return super.canPerformAction(action, withSender: sender)
     }
 }
