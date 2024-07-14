3e9d8c033da8bf513fbe5ef5aab80a20eaf2ef76
mcrkgus <mcrkgus@naver.com>
2024-07-15 04:18:37 +0900

    [Feat] #64 - TotalListBottomSheet View로 전환

--- Hankkijogbo/Hankkijogbo/Present/Home/View/TotalListBottomSheetView.swift
+++ Hankkijogbo/Hankkijogbo/Present/Home/View/TotalListBottomSheetView.swift
@@ -9,24 +9,93 @@ import UIKit
 
 final class TotalListBottomSheetView: BaseView {
     
-    let hankkiCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    let data = dummyData
+    
+   // let hankkiCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let flowLayout = UICollectionViewFlowLayout()
+    private lazy var totalListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupDelegate()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     override func setupHierarchy() {
-        addSubview(hankkiCollectionView)
+        addSubview(totalListCollectionView)
     }
     
     override func setupStyle() {
-        hankkiCollectionView.do {
-            $0.backgroundColor = .white
+        
+        flowLayout.do {
+            $0.estimatedItemSize = .init(width: UIScreen.getDeviceWidth(), height: 56)
+            $0.minimumLineSpacing = 12
+            $0.scrollDirection = .vertical
+        }
+        
+        totalListCollectionView.do {
+            $0.backgroundColor = .gray100
             $0.register(TotalListCollectionViewCell.self, forCellWithReuseIdentifier: TotalListCollectionViewCell.className)
             $0.dragInteractionEnabled = true
         }
     }
     
     override func setupLayout() {
-        hankkiCollectionView.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(20)
+        totalListCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
 
     }
 }
+
+extension TotalListBottomSheetView {
+    func setupDelegate() {
+        totalListCollectionView.delegate = self
+        totalListCollectionView.dataSource = self
+    }
+}
+
+extension TotalListBottomSheetView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TotalListCollectionViewCell.className, for: indexPath) as? TotalListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let model = data[indexPath.row]
+        cell.bindData(model: model)
+        cell.makeRounded(radius: 10)
+     //   cell.delegate = self
+        return cell
+    }
+    
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension TotalListBottomSheetView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("CELL CLICK")
+    }
+}
+
+extension TotalListBottomSheetView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 331, height: 104)
+    }
+}
+
+//extension TotalListBottomSheetView: TotalListCollectionViewCellDelegate {
+//    func addButtonDidTap(in cell: TotalListCollectionViewCell) {
+//        let myzipVC = MyZipListBottomSheetViewController()
+//        myzipVC.modalPresentationStyle = .fullScreen
+//        present(myzipVC, animated: true, completion: nil)
+//    }
+//}
