0c2e6a0231513e475b4583ca2cc640151822392d
mcrkgus <mcrkgus@naver.com>
2024-07-16 02:16:39 +0900

    [Network] #68 - HankkiListDTO: GET

--- Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetHankkiListResponseDTO.swift
+++ Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetHankkiListResponseDTO.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct GetHankkiListResponseDTO: Codable {
+    let code: Int
+    let message: String
+    let data: GetHankkiListResponseData
+}
+
+struct GetHankkiListResponseData: Codable {
+    let stores: [GetHankkiListData]
+}
+
+struct GetHankkiListData: Codable {
+    let imageUrl: String
+    let category: String
+    let name: String
+    let lowestPrice: Int
+    let heartCount: Int
+    let id: Int
+}
