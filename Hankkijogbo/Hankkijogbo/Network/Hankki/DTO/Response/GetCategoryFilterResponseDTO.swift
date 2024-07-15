f5f183138d113d0d6770c6763803a68b7d1ab436
mcrkgus <mcrkgus@naver.com>
2024-07-16 02:17:13 +0900

    [Network] #68 - CategoryFilterDTO: GET

--- Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetCategoryFilterResponseDTO.swift
+++ Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetCategoryFilterResponseDTO.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct GetCategoryFilterResponseDTO: Codable {
+    let code: Int
+    let message: String
+    let data: GetCategoryFilterResponseData
+}
+
+struct GetCategoryFilterResponseData: Codable {
+    let categories: [GetCategoryFilterData]
+}
+
+struct GetCategoryFilterData: Codable {
+    let name: String
+    let tag: String
+    let imageUrl: String
+}
