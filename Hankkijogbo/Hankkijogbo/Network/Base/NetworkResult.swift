69d57253759c059fffcfb26e8ecdeceeefd4be16
mcrkgus <mcrkgus@naver.com>
2024-07-16 23:26:56 +0900

    [Chore] #68 - 코드리뷰 반영

--- Hankkijogbo/Hankkijogbo/Network/Base/NetworkResult.swift
+++ Hankkijogbo/Hankkijogbo/Network/Base/NetworkResult.swift
@@ -12,28 +12,28 @@ enum NetworkResult<T> {
     case success(T?)
     
     case networkFail        // 네트워크 연결 실패했을 때
-    case decodeErr          // 데이터는 받아왔으나 DTO 형식으로 decode가 되지 않을 때
+    case decodeError          // 데이터는 받아왔으나 DTO 형식으로 decode가 되지 않을 때
     
     case badRequest         // BAD REQUEST EXCEPTION (400)
     case unAuthorized       // UNAUTHORIZED EXCEPTION (401)
     case notFound           // NOT FOUND (404)
     case unProcessable      // UNPROCESSABLE_ENTITY (422)
-    case serverErr          // INTERNAL_SERVER_ERROR (500번대)
-    case pathErr
+    case serverError          // INTERNAL_SERVER_ERROR (500번대)
+    case pathError
     
     var stateDescription: String {
         switch self {
         case .success: return "🍚🔥 SUCCESS 🔥🍚"
 
         case .networkFail: return "🍚🔥 NETWORK FAIL 🔥🍚"
-        case .decodeErr: return "🍚🔥 DECODED_ERROR 🔥🍚"
+        case .decodeError: return "🍚🔥 DECODED_ERROR 🔥🍚"
             
         case .badRequest: return "🍚🔥 BAD REQUEST EXCEPTION 🔥🍚"
         case .unAuthorized: return "🍚🔥 UNAUTHORIZED EXCEPTION 🔥🍚"
         case .notFound: return "🍚🔥 NOT FOUND 🔥🍚"
         case .unProcessable: return "🍚🔥 UNPROCESSABLE ENTITY 🔥🍚"
-        case .serverErr: return "🍚🔥 INTERNAL SERVER_ERROR 🔥🍚"
-        case .pathErr: return "🍚🔥 PATH ERROR 🔥🍚"
+        case .serverError: return "🍚🔥 INTERNAL SERVER_ERROR 🔥🍚"
+        case .pathError: return "🍚🔥 PATH ERROR 🔥🍚"
         }
     }
 }
