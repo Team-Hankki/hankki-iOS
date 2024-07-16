0eac257eea3599d449077e3cd9f574faf4f511a7
mcrkgus <mcrkgus@naver.com>
2024-07-17 02:36:46 +0900

    [Network] #75 - SortOptionFilterDTO: Get

--- Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetSortOptionFilterResponseDTO.swift
+++ Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetSortOptionFilterResponseDTO.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct GetSortOptionFilterResponseDTO: Codable {
+    let code: Int
+    let message: String
+    let data: GetSortOptionFilterResponseData
+}
+
+struct GetSortOptionFilterResponseData: Codable {
+    let priceCategories: [GetSortOptionFilterData]
+}
+
+struct GetSortOptionFilterData: Codable {
+    let name: String
+    let tag: String
+}
