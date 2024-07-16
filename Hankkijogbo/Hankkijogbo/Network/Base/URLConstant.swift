b0ab1d5f77fb6528d45cc4cfd6726a2c21c4b6c0
mcrkgus <mcrkgus@naver.com>
2024-07-16 15:07:30 +0900

    [Network] #68 - URL Constant 생성

---
+++ Hankkijogbo/Hankkijogbo/Network/Base/URLConstant.swift
@@ -0,0 +1,19 @@
+//
+//  URLConstant.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/16/24.
+//
+
+import Foundation
+
+enum URLConstant {
+    
+    // MARK: - Base URL
+    
+    static let baseURL = Config.baseURL
+    
+    // MARK: - URL Path
+    
+    static let bearer = "Bearer "
+}
