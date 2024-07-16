b3f249f60fd191733a1b764b5a9ae4d42880c155
mcrkgus <mcrkgus@naver.com>
2024-07-17 05:59:26 +0900

    [Network] #75 - 가격 필터 드롭다운 정보 조회 API Service

--- Hankkijogbo/Hankkijogbo/Network/Hankki/HakkiAPIService.swift
+++ Hankkijogbo/Hankkijogbo/Network/Hankki/HakkiAPIService.swift
@@ -10,14 +10,15 @@ import Moya
 
 protocol HankkiAPIServiceProtocol {
     func getCategoryFilter(completion: @escaping(NetworkResult<GetCategoryFilterResponseDTO>) -> Void)
-//    func getPriceCategoryFilter(completion: @escaping(NetworkResult<GetPriceCategoryFilterResponseDTO>) -> Void)
-//    func getSortOptionFilter(completion: @escaping(NetworkResult<GetSortOptionResponseDTO>) -> Void)
-//    func getHankkiPin(completion: @escaping(NetworkResult<GetHankkiPinResponseDTO>) -> Void)
+    func getPriceCategoryFilter(completion: @escaping(NetworkResult<GetPriceFilterResponseDTO>) -> Void)
+   // func getSortOptionFilter(completion: @escaping(NetworkResult<GetSortOptionFilterResponseDTO>) -> Void)
 }
 
 final class HankkiAPIService: BaseAPIService, HankkiAPIServiceProtocol {
     
     private let provider = MoyaProvider<HankkiTargetType>(plugins: [MoyaPlugin()])
+    
+    /// 카테고리 필터 드롭다운 정보 조회
     func getCategoryFilter(completion: @escaping (NetworkResult<GetCategoryFilterResponseDTO>) -> Void) {
         provider.request(.getCategoryFilter) { result in
             switch result {
@@ -33,4 +34,23 @@ final class HankkiAPIService: BaseAPIService, HankkiAPIServiceProtocol {
             }
         }
     }
+    
+    /// 가격 필터 드롭다운 정보 조회
+    func getPriceCategoryFilter(completion: @escaping (NetworkResult<GetPriceFilterResponseDTO>) -> Void) {
+        provider.request(.getPriceCategoryFilter) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<GetPriceFilterResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                print(networkResult.stateDescription)
+                completion(networkResult)
+            case .failure(let error):
+                if let response = error.response {
+                    let networkResult: NetworkResult<GetPriceFilterResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                    completion(networkResult)
+                }
+            }
+        }
+    }
+    
+    
 }
