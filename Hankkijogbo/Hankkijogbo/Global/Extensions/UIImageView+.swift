810e67d4338bc6b33731b1015694bb618e8c4e5a
mcrkgus <mcrkgus@naver.com>
2024-07-17 16:04:19 +0900

    [Feat] #84 - UIImageView Extension KingFisher 사용

---
+++ Hankkijogbo/Hankkijogbo/Global/Extensions/UIImageView+.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/17/24.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView{
+    func kfSetImage(url : String?){
+        
+        guard let url = url else { return }
+        
+        if let url = URL(string: url) {
+            kf.indicatorType = .activity
+            kf.setImage(with: url,
+                        placeholder: nil,
+                        options: [.transition(.fade(1.0))], progressBlock: nil)
+        }
+    }
+}
