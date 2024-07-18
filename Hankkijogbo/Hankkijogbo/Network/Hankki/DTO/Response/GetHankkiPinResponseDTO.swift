2c596938a7c41fd09bc2afe4db33a253aa1ea69f
mcrkgus <mcrkgus@naver.com>
2024-07-18 16:25:35 +0900

    [Network] #98 - 메인 페이지 식당 핀 조회 DTO

---
+++ Hankkijogbo/Hankkijogbo/Network/Hankki/DTO/Response/GetHankkiPinResponseDTO.swift
@@ -0,0 +1,21 @@
+//
+//  GetHankkiPinResponseDTO.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 7/18/24.
+//
+
+import Foundation
+
+/// 메인 페이지 식당 핀 조회
+
+struct GetHankkiPinResponseData: Codable {
+    let pins: [GetHankkiPinData]
+}
+
+struct GetHankkiPinData: Codable {
+    let latitude: Double
+    let longitude: Double
+    let id: Int
+    let name: String
+}
