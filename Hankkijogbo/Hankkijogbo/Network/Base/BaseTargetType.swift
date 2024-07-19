14ff734e5ab9c7d61f9e73df8eb78fbe2ef25340
shimseohyun <shim020924@gmail.com>
2024-07-19 17:31:05 +0900

    [Fix] #119 - Header 수정

--- Hankkijogbo/Hankkijogbo/Network/Base/BaseTargetType.swift
+++ Hankkijogbo/Hankkijogbo/Network/Base/BaseTargetType.swift
@@ -64,11 +64,12 @@ extension BaseTargetType {
         case .withdrawHeader(let authorizationCode):
             header["Content-Type"] = "application/json"
             let accessToken = UserDefaults.standard.getAccesshToken()
-            header["Authorization"] = "Bearer \(accessToken)"
+            header["Authorization"] = URLConstant.bearer + "\(accessToken)"
             header["X-Apple-Code"] = "\(authorizationCode)"
         case .formdataHeader:
             header["Content-Type"] = "multipart/form-data"
-            header["Authorization"] = URLConstant.bearer
+            let accessToken = UserDefaults.standard.getAccesshToken()
+            header["Authorization"] = URLConstant.bearer + "\(accessToken)"
         default:
             header["Content-Type"] = "application/json"
             let accessToken = UserDefaults.standard.getAccesshToken()
