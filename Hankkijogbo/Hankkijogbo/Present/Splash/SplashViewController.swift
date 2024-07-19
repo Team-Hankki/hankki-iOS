7e22ae43debf5facff7c3fdddaba7a31a632bb8d
shimseohyun <shim020924@gmail.com>
2024-07-19 14:47:23 +0900

    [Feat] #112 - Splash View

--- Hankkijogbo/Hankkijogbo/Present/Splash/SplashViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/Splash/SplashViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 final class SplashViewController: BaseViewController {
+    // MARK: - UI Components
+    
+    let titleLabel: UILabel = UILabel()
+    let logoImageView: UIImageView = UIImageView()
+    let logoTextView: UIImageView = UIImageView()
+    
+    // MARK: - Life Cycle
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -18,8 +26,44 @@ final class SplashViewController: BaseViewController {
         }
     }
     
-    override func viewDidLoad() {
-        view.backgroundColor = .yellow
+    // MARK: - setup UI
+    
+    override func setupStyle() {
+        titleLabel.do {
+            $0.attributedText = UILabel.setupAttributedText(for: PretendardStyle.subtitle1, 
+                                                            withText: "함께 만드는 8000원 이하 식당 지도",
+                                                            color: .gray400)
+            $0.numberOfLines = 1
+        }
+        
+        logoTextView.do {
+            $0.image = .imgSplashTextLogo
+        }
+        
+        logoImageView.do {
+            $0.image = .imgSplashImageLogo
+        }
+    }
+    
+    override func setupHierarchy() {
+        view.addSubviews(titleLabel, logoTextView, logoImageView)
+    }
+    
+    override func setupLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(UIScreen.convertByHeightRatio(190))
+            $0.centerX.equalToSuperview()
+        }
+        
+        logoTextView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(14)
+            $0.centerX.equalToSuperview()
+        }
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalTo(logoTextView.snp.bottom).offset(42)
+            $0.centerX.equalToSuperview()
+        }
     }
 }
 
@@ -29,13 +73,17 @@ private extension SplashViewController {
     }
     
     func presentHomeView() {
-        let navigationController = HankkiNavigationController(rootViewController: TabBarController())
-        self.view.window?.rootViewController = navigationController
-        navigationController.popToRootViewController(animated: false)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            let navigationController = HankkiNavigationController(rootViewController: TabBarController())
+            self.view.window?.rootViewController = navigationController
+            navigationController.popToRootViewController(animated: false)
+        }
     }
     
     func presentOnboardingView() {
-        self.view.window?.rootViewController = OnboardingViewController()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.view.window?.rootViewController = OnboardingViewController()
+        }
     }
 }
 
