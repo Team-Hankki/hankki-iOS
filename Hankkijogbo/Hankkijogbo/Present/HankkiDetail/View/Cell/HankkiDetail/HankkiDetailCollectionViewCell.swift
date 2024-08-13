5e8fc7e2167dc66512bce86f3b8d527528b48d62
EunsuSeo01 <eunsu0613@naver.com>
2024-08-13 17:20:51 +0900

    [Fix] #164 - 메뉴 이름 12글자 이상일 때 크롭 및 글자수에 따른 Dot line 레이아웃 동적 변경

--- Hankkijogbo/Hankkijogbo/Present/HankkiDetail/View/Cell/HankkiDetail/HankkiDetailCollectionViewCell.swift
+++ Hankkijogbo/Hankkijogbo/Present/HankkiDetail/View/Cell/HankkiDetail/HankkiDetailCollectionViewCell.swift
@@ -9,7 +9,11 @@ import UIKit
 
 final class HankkiDetailCollectionViewCell: BaseCollectionViewCell {
     
-    // MARK: - UI Properties
+    // MARK: - Properties
+    
+    private let menuNameMaxLength = 12
+    
+    // MARK: - UI Components
     
     private var hankkiMenuNameLabel: UILabel = UILabel()
     private let dottedLineView: DottedLineView = DottedLineView(frame: CGRect(x: 0, y: 0, width: 0, height: 4))
@@ -59,9 +63,38 @@ final class HankkiDetailCollectionViewCell: BaseCollectionViewCell {
     }
 }
 
+private extension HankkiDetailCollectionViewCell {
+    /// 메뉴 이름 길이에 따라 label text를 다르게 업데이트
+    func updateMenuNameLabel(name: String) {
+        if name.count > menuNameMaxLength {
+            cropMenuNameByMaxLength(name: name)
+        } else {
+            hankkiMenuNameLabel.text = name
+        }
+    }
+    
+    /// 최대 길이를 넘는다면 crop
+    func cropMenuNameByMaxLength(name: String) {
+        let index = name.index(name.startIndex, offsetBy: menuNameMaxLength)
+        hankkiMenuNameLabel.text = String(name[..<index]) + "..."
+    }
+    
+    /// 메뉴 이름 길이에 맞게 dottedLineView의 레이아웃 업데이트
+    func updateDottedLineViewLayout(by menuName: String) {
+        var length = min(menuName.count, menuNameMaxLength)
+        dottedLineView.snp.updateConstraints {
+            $0.leading.equalTo(hankkiMenuNameLabel.snp.trailing).offset(27 - (length - 1) * 2)
+            $0.trailing.equalTo(hankkiMenuPriceLabel.snp.leading).offset(-(27 - (length - 1) * 2))
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(2)
+        }
+    }
+}
+
 extension HankkiDetailCollectionViewCell {
     func bindMenuData(_ menuData: MenuData) {
-        hankkiMenuNameLabel.text = menuData.name
+        updateMenuNameLabel(name: menuData.name)
         hankkiMenuPriceLabel.text = "\(menuData.price)\(StringLiterals.Common.won)"
+        updateDottedLineViewLayout(by: menuData.name)
     }
 }
