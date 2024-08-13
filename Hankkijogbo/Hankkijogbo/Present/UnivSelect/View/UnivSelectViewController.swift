4f930bd2b862bb00b41aeacab6ba4bc840f21b66
mcrkgus <mcrkgus@naver.com>
2024-08-14 08:52:04 +0900

    [Chore] #176 - 중복 제거

--- Hankkijogbo/Hankkijogbo/Present/UnivSelect/View/UnivSelectViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/UnivSelect/View/UnivSelectViewController.swift
@@ -155,8 +155,6 @@ private extension UnivSelectViewController {
             // 위치 정보 접근이 거부된 경우, 서울시립대학교로 포커싱
             viewModel.currentUnivIndex = 24
             viewModel.postMeUniversity()
-            navigationController?.popViewController(animated: true)
-            
         case .notDetermined:
             // 위치 정보 접근 권한이 아직 결정되지 않은 경우 동의 요청
             if let homeVC = self.navigationController?.viewControllers.first(where: { $0 is HomeViewController }) as? HomeViewController {
