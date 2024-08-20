84d4bcc1c5980cd8e9529fdc37e6684bc7f31ff4
shimseohyun <shim020924@gmail.com>
2024-08-21 04:52:37 +0900

    [Fix] #195 - Detail Liked  관련 로직 수정

--- Hankkijogbo/Hankkijogbo/Present/HankkiList/ViewModel/HankkiListViewModel.swift
+++ Hankkijogbo/Hankkijogbo/Present/HankkiList/ViewModel/HankkiListViewModel.swift
@@ -11,7 +11,7 @@ import UIKit
 final class HankkiListViewModel {
     
     var reloadCollectionView: (() -> Void)?
-    
+    var firstHankkiList: [HankkiListTableViewCell.Model] = []
     var hankkiList: [HankkiListTableViewCell.Model] = [] {
         didSet {
             self.reloadCollectionView?()
@@ -21,6 +21,44 @@ final class HankkiListViewModel {
 }
 
 extension HankkiListViewModel {
+    func updateMeHankkiList() {
+        let prevHankkiList: [HankkiListTableViewCell.Model] = hankkiList
+        
+        NetworkService.shared.userService.getMeHankkiList(.getMeHankkiHeartList) { result in
+            result.handleNetworkResult { response in
+                let currentHankkiList: [HankkiListTableViewCell.Model] = response.data.stores.map {
+                    return HankkiListTableViewCell.Model(id: $0.id,
+                                                         name: $0.name,
+                                                         imageURL: $0.imageUrl ?? "",
+                                                         category: $0.category,
+                                                         lowestPrice: $0.lowestPrice,
+                                                         heartCount: $0.heartCount)
+                }
+                
+                // 현재 목록의 id 리스트를 Set으로 변환
+                let currentIds = Set(currentHankkiList.map { $0.id })
+                let heartCountDict = Dictionary(uniqueKeysWithValues: currentHankkiList.map { ($0.id, $0.heartCount) })
+                
+                let updatedHankkiList = self.firstHankkiList.map {
+                    var store = $0
+                    if !currentIds.contains(store.id) {
+                        // 하트를 지웠을 경우
+                        if !store.isDeleted {
+                            store.isDeleted = true
+                            store.heartCount -= 1
+                        }
+                    } else {
+                        // 다시 하트를 눌렀을 경우
+                        store.isDeleted = false
+                        store.heartCount = heartCountDict[store.id] ?? store.heartCount
+                    }
+                    return store
+                }
+                self.hankkiList = updatedHankkiList
+            }
+        }
+    }
+    
     func getZipDetail(zipId: Int) {
         NetworkService.shared.zipService.getZipList(zipId: zipId) { result in
             
@@ -52,6 +90,8 @@ extension HankkiListViewModel {
                                                          lowestPrice: $0.lowestPrice,
                                                          heartCount: $0.heartCount)
                 }
+                
+                self.firstHankkiList = self.hankkiList
             }
         }
     }
