8802c8529536a6d4cfe1de5ca0f1a97e85da8b09
mcrkgus <mcrkgus@naver.com>
2024-10-13 01:42:35 +0900

    [Feat] #274 - Network Result Delegate

---
+++ Hankkijogbo/Hankkijogbo/Network/Base/NetworkResultDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkResultDelegate.swift
+//  Hankkijogbo
+//
+//  Created by Gahyun Kim on 10/13/24.
+//
+
+import Foundation
+
+protocol NetworkResultDelegate: AnyObject {
+    func moveToLoginScreen()
+}
