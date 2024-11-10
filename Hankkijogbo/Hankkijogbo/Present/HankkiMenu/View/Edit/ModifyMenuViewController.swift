eb7e5179e54d2a289ebdee2f3ecc9829fbbff824
EunsuSeo01 <eunsu0613@naver.com>
2024-11-10 19:22:15 +0900

    [Fix] #273 - originalText 파라미터 값 채우기

--- Hankkijogbo/Hankkijogbo/Present/HankkiMenu/View/Edit/ModifyMenuViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/HankkiMenu/View/Edit/ModifyMenuViewController.swift
@@ -20,11 +20,13 @@ final class ModifyMenuViewController: BaseViewController {
     private let titleLabel: UILabel = UILabel()
     private lazy var nameTextField: ModifyMenuTextField = ModifyMenuTextField(
         titleText: StringLiterals.ModifyMenu.name,
+        originalText: viewModel.selectedMenu.name,
         placeholderText: StringLiterals.ModifyMenu.namePlaceholder,
         hankkiTextFieldDelegate: self
     )
     private lazy var priceTextField: ModifyMenuTextField = ModifyMenuTextField(
         titleText: StringLiterals.ModifyMenu.price,
+        originalText: String(viewModel.selectedMenu.price),
         hankkiTextFieldDelegate: self
     )
     private let over8000PriceLabel: UILabel = UILabel()
