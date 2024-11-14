1f8f19ff3473352a289c8ac8766a3cbe3ce33e8c
EunsuSeo01 <eunsu0613@naver.com>
2024-11-15 04:24:01 +0900

    [Fix] #289 - Response DTO 수정
    서버가 dto 구조 하나로 통일해줘서 반영함

--- Hankkijogbo/Hankkijogbo/Network/Menu/MenuAPIService.swift
+++ Hankkijogbo/Hankkijogbo/Network/Menu/MenuAPIService.swift
@@ -11,11 +11,10 @@ import Moya
 
 protocol MenuAPIServiceProtocol {
     
-    typealias GetMenuResponseDTO = BaseDTO<GetMenuResponseData>
-    typealias PostMenuResponseDTO = BaseDTO<PostMenuResponseData>
+    typealias MenuResponseDTO = BaseDTO<MenuResponseData>
     
-    func getMenu(storeId: Int, completion: @escaping(NetworkResult<GetMenuResponseDTO>) -> Void)
-    func postMenu(storeId: Int, requestBody: [MenuRequestDTO], completion: @escaping(NetworkResult<PostMenuResponseDTO>) -> Void)
+    func getMenu(storeId: Int, completion: @escaping(NetworkResult<MenuResponseDTO>) -> Void)
+    func postMenu(storeId: Int, requestBody: [MenuRequestDTO], completion: @escaping(NetworkResult<MenuResponseDTO>) -> Void)
     func patchMenu(storeId: Int, id: Int, requestBody: MenuRequestDTO, completion: @escaping(NetworkResult<EmptyDTO>) -> Void)
     func deleteMenu(storeId: Int, id: Int, completion: @escaping(NetworkResult<Void>) -> Void)
 }
@@ -24,15 +23,15 @@ final class MenuAPIService: BaseAPIService, MenuAPIServiceProtocol {
     
     private let provider = MoyaProvider<MenuTargetType>(plugins: [MoyaPlugin.shared])
     
-    func getMenu(storeId: Int, completion: @escaping (NetworkResult<GetMenuResponseDTO>) -> Void) {
+    func getMenu(storeId: Int, completion: @escaping (NetworkResult<MenuResponseDTO>) -> Void) {
         provider.request(.getMenu(storeId: storeId)) { result in
             switch result {
             case .success(let response):
-                let networkResult: NetworkResult<GetMenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                let networkResult: NetworkResult<MenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
                 completion(networkResult)
             case .failure(let error):
                 if let response = error.response {
-                    let networkResult: NetworkResult<GetMenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                    let networkResult: NetworkResult<MenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
                     completion(networkResult)
                 }
             }
@@ -42,16 +41,16 @@ final class MenuAPIService: BaseAPIService, MenuAPIServiceProtocol {
     func postMenu(
         storeId: Int,
         requestBody: [MenuRequestDTO],
-        completion: @escaping (NetworkResult<PostMenuResponseDTO>) -> Void
+        completion: @escaping (NetworkResult<MenuResponseDTO>) -> Void
     ) {
         provider.request(.postMenu(storeId: storeId, requestBody: requestBody)) { result in
             switch result {
             case .success(let response):
-                let networkResult: NetworkResult<PostMenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                let networkResult: NetworkResult<MenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
                 completion(networkResult)
             case .failure(let error):
                 if let response = error.response {
-                    let networkResult: NetworkResult<PostMenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
+                    let networkResult: NetworkResult<MenuResponseDTO> = self.fetchNetworkResult(statusCode: response.statusCode, data: response.data)
                     completion(networkResult)
                 }
             }
