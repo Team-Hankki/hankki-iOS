aa2fb9807236fa6dab5d94f240624bf51ee77c2b
EunsuSeo01 <eunsu0613@naver.com>
2024-12-19 20:41:20 +0900

    [Chore] #313 - 필요없어진 뷰 제거

--- Hankkijogbo/Hankkijogbo/Present/HankkiDetail/View/HankkiDetailViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/HankkiDetail/View/HankkiDetailViewController.swift
@@ -27,8 +27,6 @@ final class HankkiDetailViewController: BaseViewController, NetworkResultDelegat
     private let backButton: UIButton = UIButton()
     private let thumbnailImageView: UIImageView = UIImageView()
     private let topBlackGradientImageView: UIImageView = UIImageView()
-    private var infoCollectionView: HankkiDetailCollectionView = HankkiDetailCollectionView()
-    private var reportOptionCollectionView: HankkiReportOptionCollectionView = HankkiReportOptionCollectionView()
     
     // MARK: - Life Cycle
     
@@ -73,9 +71,7 @@ final class HankkiDetailViewController: BaseViewController, NetworkResultDelegat
         contentView.addSubviews(
             lightGrayBackgroundView,
             thumbnailImageView,
-            backButton,
-            infoCollectionView,
-            reportOptionCollectionView
+            backButton
         )
         thumbnailImageView.addSubview(topBlackGradientImageView)
     }
@@ -105,15 +101,6 @@ final class HankkiDetailViewController: BaseViewController, NetworkResultDelegat
             $0.top.equalToSuperview().inset(UIApplication.getStatusBarHeight() + 15)
             $0.leading.equalToSuperview().inset(20)
             $0.size.equalTo(20)
-        }
-        infoCollectionView.snp.makeConstraints {
-            $0.top.equalTo(thumbnailImageView.snp.bottom).offset(-37)
-            $0.centerX.equalToSuperview()
-        }
-        reportOptionCollectionView.snp.makeConstraints {
-            $0.top.equalTo(infoCollectionView.snp.bottom).offset(50)
-            $0.horizontalEdges.equalToSuperview().inset(22)
-            $0.bottom.equalToSuperview().inset(31)
         }
     }
     
@@ -145,8 +132,6 @@ private extension HankkiDetailViewController {
                 } else {
                     self?.setupNoImageStyle()
                 }
-                self?.infoCollectionView.updateLayout(menuSize: data.menus.count)
-                self?.infoCollectionView.collectionView.reloadData()
             }
         }
         
@@ -162,42 +147,11 @@ private extension HankkiDetailViewController {
     }
     
     func setupRegister() {
-        infoCollectionView.collectionView.do {
-            $0.register(HankkiDetailCollectionViewCell.self, forCellWithReuseIdentifier: HankkiDetailCollectionViewCell.className)
-            $0.register(HankkiEditMenuCollectionViewCell.self, forCellWithReuseIdentifier: HankkiEditMenuCollectionViewCell.className)
-            $0.register(
-                HankkiDetailHeaderView.self,
-                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                withReuseIdentifier: HankkiDetailHeaderView.className
-            )
-            $0.register(
-                HankkiDetailFooterView.self,
-                forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                withReuseIdentifier: HankkiDetailFooterView.className
-            )
-        }
-        
-        reportOptionCollectionView.collectionView.do {
-            $0.register(HankkiReportOptionCollectionViewCell.self, forCellWithReuseIdentifier: HankkiReportOptionCollectionViewCell.className)
-            $0.register(
-                HankkiReportOptionHeaderView.self,
-                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
-                withReuseIdentifier: HankkiReportOptionHeaderView.className
-            )
-            $0.register(
-                HankkiReportOptionFooterView.self,
-                forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter,
-                withReuseIdentifier: HankkiReportOptionFooterView.className
-            )
-        }
+        // 메뉴판 컬뷰 예정
     }
     
     func setupDelegate() {
         scrollView.delegate = self
-        infoCollectionView.collectionView.delegate = self
-        infoCollectionView.collectionView.dataSource = self
-        reportOptionCollectionView.collectionView.delegate = self
-        reportOptionCollectionView.collectionView.dataSource = self
         viewModel.delegate = self
     }
     
@@ -335,134 +289,12 @@ extension HankkiDetailViewController {
     }
 }
 
-extension HankkiDetailViewController {
+extension HankkiDetailViewController: UIScrollViewDelegate {
+    
     /// 상단의 bounces만 비활성화
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    @objc func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView.contentOffset.y < -UIApplication.getStatusBarHeight() {
             scrollView.contentOffset.y = -UIApplication.getStatusBarHeight()
-        }
-    }
-}
-
-// MARK: - UICollectionView Delegate
-
-extension HankkiDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if collectionView == infoCollectionView.collectionView {
-            switch kind {
-            case UICollectionView.elementKindSectionHeader:
-                guard let header = collectionView.dequeueReusableSupplementaryView(
-                    ofKind: kind,
-                    withReuseIdentifier: HankkiDetailHeaderView.className,
-                    for: indexPath
-                ) as? HankkiDetailHeaderView else {
-                    return UICollectionReusableView()
-                }
-                if let data = viewModel.hankkiDetailData {
-                    header.bindData(name: data.name, category: data.category)
-                }
-                return header
-            case UICollectionView.elementKindSectionFooter:
-                guard let footer = collectionView.dequeueReusableSupplementaryView(
-                    ofKind: kind,
-                    withReuseIdentifier: HankkiDetailFooterView.className,
-                    for: indexPath
-                ) as? HankkiDetailFooterView else {
-                    return UICollectionReusableView()
-                }
-                footer.likedButton.buttonHandler = {
-                    if !footer.isLiked {
-                        self.viewModel.postHankkiHeartAPI(id: self.hankkiId) {
-                            footer.updateLikeButtonStatus()
-                        }
-                    } else {
-                        self.viewModel.deleteHankkiHeartAPI(id: self.hankkiId) {
-                            footer.updateLikeButtonStatus()
-                        }
-                    }
-                }
-                if let data = viewModel.hankkiDetailData {
-                    footer.isLiked = data.isLiked
-                    footer.likedNumber = data.heartCount
-                }
-                footer.addMyZipButton.hankkiDetailButton.addTarget(self, action: #selector(addMyZipButtonDidTap), for: .touchUpInside)
-                return footer
-            default:
-                return UICollectionReusableView()
-            }
-        } else if collectionView == reportOptionCollectionView.collectionView {
-            switch kind {
-            case UICollectionView.elementKindSectionHeader:
-                guard let header = collectionView.dequeueReusableSupplementaryView(
-                    ofKind: kind,
-                    withReuseIdentifier: HankkiReportOptionHeaderView.className,
-                    for: indexPath
-                ) as? HankkiReportOptionHeaderView else {
-                    return UICollectionReusableView()
-                }
-                return header
-            case UICollectionView.elementKindSectionFooter:
-                guard let footer = collectionView.dequeueReusableSupplementaryView(
-                    ofKind: kind,
-                    withReuseIdentifier: HankkiReportOptionFooterView.className,
-                    for: indexPath
-                ) as? HankkiReportOptionFooterView else {
-                    return UICollectionReusableView()
-                }
-                return footer
-            default:
-                return UICollectionReusableView()
-            }
-        }
-        return UICollectionReusableView()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == infoCollectionView.collectionView {
-            (viewModel.hankkiDetailData?.menus.count ?? 0) + 1
-        } else {
-            reportOptionArray.count
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == infoCollectionView.collectionView {
-            if indexPath.item == viewModel.hankkiDetailData?.menus.count {
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HankkiEditMenuCollectionViewCell.className, for: indexPath) as? HankkiEditMenuCollectionViewCell else { return UICollectionViewCell() }
-                cell.editMenuButton.addTarget(self, action: #selector(editMenuButtonDidTap), for: .touchUpInside)
-                return cell
-            } else {
-                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HankkiDetailCollectionViewCell.className, for: indexPath) as? HankkiDetailCollectionViewCell else { return UICollectionViewCell() }
-                if let data = viewModel.hankkiDetailData {
-                    cell.bindMenuData(data.menus[indexPath.item])
-                }
-                return cell
-            }
-        } else if collectionView == reportOptionCollectionView.collectionView {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HankkiReportOptionCollectionViewCell.className, for: indexPath) as? HankkiReportOptionCollectionViewCell else { return UICollectionViewCell() }
-            cell.delegate = self
-            cell.bindData(text: reportOptionArray[indexPath.item])
-            return cell
-        }
-        return UICollectionViewCell()
-    }
-}
-
-// MARK: - UpdateReportButtonStyle Delegate
-
-extension HankkiDetailViewController: UpdateReportButtonStyleDelegate {
-    func updateReportButtonStyle(isEnabled: Bool) {
-        guard let footer = reportOptionCollectionView.collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: HankkiReportOptionFooterView.className, for: IndexPath(item: 3, section: 0)) as? HankkiReportOptionFooterView else { return }
-        
-        if isEnabled {
-            footer.hankkiReportButton.setupIsValid(true)
-            footer.hankkiReportButton.addTarget(
-                self,
-                action: #selector(hankkiReportButtonDidTap),
-                for: .touchUpInside
-            )
-        } else {
-            footer.hankkiReportButton.setupIsValid(false)
         }
     }
 }
