f816dc7034acdc5a971d4535d6e5c368d8774d39
mcrkgus <mcrkgus@naver.com>
2025-02-27 12:11:18 +0900

    [Chore] #336 - del codes

--- Hankkijogbo/Hankkijogbo/Present/Home/View/HomeViewController.swift
+++ Hankkijogbo/Hankkijogbo/Present/Home/View/HomeViewController.swift
@@ -110,8 +110,6 @@ final class HomeViewController: BaseViewController, NetworkResultDelegate {
                 self.rootView.bottomSheetView.data = data
                 self.rootView.bottomSheetView.updateTotalListCount(count: data.count)
                 self.rootView.bottomSheetView.totalListCollectionView.reloadData()
-//                self.rootView.bottomSheetView.setNeedsLayout()
-//                self.rootView.bottomSheetView.layoutIfNeeded()
             }
         }
         
